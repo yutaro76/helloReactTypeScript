386c9f2442135d2094253d287317b372e7c746e0
yutaro okuda <yutaro.okuda.76@gmail.com>
2023-12-31 11:15:23 +0800

    first commit

---
+++ src/Helloworld.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Helloworld(){
+    return (
+        <div className="Helloworld">
+            <p>Hello, world!</p>
+            <p>react & typescript</p>
+        </div>
+    );
+}
+
+export default Helloworld;
